b30f9db6f73f0e7e3589630ba4adc8c9d9e315de
bruno <brunotfatima@gmail.com>
2024-01-28 11:12:01 +0000

    Constants file

---
+++ Scripts/Constants.cs
@@ -0,0 +1,6 @@
+public static class Constants
+{
+    public const string EXPRESSION_ANGRY = "angry";
+    public const string EXPRESSION_HAPPY = "happy";
+    public const string EXPRESSION_NEUTRAL = "neutral";
+}
